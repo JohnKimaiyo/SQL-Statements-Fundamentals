ee73195265cfa09d006f8b12be6119afbe454e01
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-28 20:21:46 +0300

    select databaselog

---
+++ 1  SELECT & FROM/databasdelog.sql
@@ -0,0 +1,10 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT DatabaseLogID
+      ,[PostTime]
+     --,[DatabaseUser]
+      --,[Event]
+     -- ,[Schema]
+     -- ,[Object]
+     -- ,[TSQL]
+     -- ,[XmlEvent]
+  FROM [AdventureWorksDW2019].[dbo].[DatabaseLog]
