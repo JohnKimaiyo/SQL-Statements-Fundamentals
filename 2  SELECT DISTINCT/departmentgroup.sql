9462935e0b4b3e11d781b3d06383977b862fcce7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 13:19:04 +0300

    department group sql

---
+++ 2  SELECT DISTINCT/departmentgroup.sql
@@ -0,0 +1,5 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT DISTINCT  --,[DepartmentGroupKey]
+    --  ,[ParentDepartmentGroupKey]
+      [DepartmentGroupName]
+  FROM [AdventureWorksDW2019].[dbo].[DimDepartmentGroup]
