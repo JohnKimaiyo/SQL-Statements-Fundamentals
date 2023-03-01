e07cf7ef4dc7a12950c621898544d6f1e4509807
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 12:40:42 +0300

    distinct account dbo

---
+++ 2  SELECT DISTINCT/account.sql
@@ -0,0 +1,12 @@
+
+SELECT   DISTINCT
+      --[ParentAccountKey]
+     -- ,[AccountCodeAlternateKey]
+     -- ,[ParentAccountCodeAlternateKey]
+     [AccountDescription]
+     -- ,[AccountType]
+     --,[Operator]
+      --,[CustomMembers]
+     -- ,[ValueType]
+      --,CustomMemberOptions
+  FROM [AdventureWorksDW2019].[dbo].[DimAcc
