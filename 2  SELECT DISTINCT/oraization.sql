1c0a1033226dbdb20b900ab3ad17aff7d79cce21
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 15:59:42 +0300

    organization sql dstinct

---
+++ 2  SELECT DISTINCT/oraization.sql
@@ -0,0 +1,7 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT DISTINCT --,[OrganizationKey]
+      --,[ParentOrganizationKey]
+      --,[PercentageOfOwnership]
+      OrganizationName
+     -- ,[CurrencyKey]
+  FROM [AdventureWorksDW2019].[dbo].[DimOrgani
