e2dfa8a1e62311541248245d10692920463f918f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 11:51:54 +0300

    fianace sql

---
+++ 1  SELECT & FROM/finance.sql
@@ -0,0 +1,10 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [FinanceKey]
+    --  ,[DateKey]
+    --  ,[OrganizationKey]
+    --  ,[DepartmentGroupKey]
+    --  ,[ScenarioKey]
+   --   ,[AccountKey]
+      ,[Amount]
+      ,[Date]
+  FROM [AdventureWorksDW2019].[dbo].[FactFinance]
