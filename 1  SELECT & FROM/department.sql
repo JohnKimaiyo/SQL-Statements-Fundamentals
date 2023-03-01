bc2e319ba92c4fffcb88674d9af13cb947dc0f07
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 09:58:20 +0300

    select department ql

---
+++ 1  SELECT & FROM/department.sql
@@ -0,0 +1,5 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [DepartmentGroupKey]
+    --  ,[ParentDepartmentGroupKey]
+      ,[DepartmentGroupName]
+  FROM [AdventureWorksDW2019].[dbo].[DimDepartmentGroup]
