da7306de558029d7587a51a26c0270a060a9687e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 09:35:58 +0300

    select sql statement

---
+++ 1  SELECT & FROM/account.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [AccountKey]
+      --,[ParentAccountKey]
+      --,[AccountCodeAlternateKey]
+     -- ,[ParentAccountCodeAlternateKey]
+      ,[AccountDescription]
+     -- ,[AccountType]
+      ,[Operator]
+     -- ,[CustomMembers]
+     -- ,[ValueType]
+     -- ,[CustomMemberOptions]
+  FROM [AdventureWorksDW2019].[dbo].[DimAccount]
