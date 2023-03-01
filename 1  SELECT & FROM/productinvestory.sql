3b248655bffe3220a5c90f9f1a62b696d07038ee
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 12:01:14 +0300

    product sales sql statement

---
+++ 1  SELECT & FROM/productinvestory.sql
@@ -0,0 +1,9 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ProductKey]
+      ,[DateKey]
+      ,[MovementDate]
+     -- ,[UnitCost]
+     -- ,[UnitsIn]
+     -- ,[UnitsOut]
+      ,[UnitsBalance]
+  FROM [AdventureWorksDW2019].[dbo].[FactProductInventory]
