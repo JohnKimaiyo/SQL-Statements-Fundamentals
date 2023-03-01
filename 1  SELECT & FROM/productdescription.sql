4e68bf71e1d01dcd77fd6729cb09fc6c2b718458
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-01 10:50:29 +0300

    select sql  product description

---
+++ 1  SELECT & FROM/productdescription.sql
@@ -0,0 +1,5 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ProductKey]
+      ,[CultureName]
+    --  ,[ProductDescription]
+  FROM [AdventureWorksDW2019].[dbo].[FactAdditionalInternati
