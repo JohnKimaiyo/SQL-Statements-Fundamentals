7377a3fe2e2e8d14441efd262237eeabb6d1c1c9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-02 11:31:52 +0300

    disticnt amount

---
+++ 2  SELECT DISTINCT/financee.sql
@@ -0,0 +1,10 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  DISTINCT --[FinanceKey]
+     -- ,[DateKey]
+     -- ,[OrganizationKey]
+     -- ,[DepartmentGroupKey]
+     --- ,[ScenarioKey]
+     -- ,[AccountKey]
+      Amount
+      --,[Date]
+  FROM [AdventureWorksDW2019].[dbo].[FactFinance]
