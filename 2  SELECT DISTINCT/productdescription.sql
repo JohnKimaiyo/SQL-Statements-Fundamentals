1f8b0dfaf2b466e8f69a514cb19a084a7ef5627a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-02 10:28:00 +0300

    sistinct culture table

---
+++ 2  SELECT DISTINCT/productdescription.sql
@@ -0,0 +1,5 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  DISTINCT -- [ProductKey]
+      CultureName
+      --,[ProductDescription]
+  FROM [AdventureWorksDW2019].[dbo].[FactAdditionalInternationalProductDescription]
