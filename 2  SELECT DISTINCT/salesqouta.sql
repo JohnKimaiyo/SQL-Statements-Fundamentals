677eeea4bcc1bd6fe9c835a5ceaf276025844723
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-02 11:59:46 +0300

    sales quota disticnt sql

---
+++ 2  SELECT DISTINCT/salesqouta.sql
@@ -0,0 +1,9 @@
+*** Script for SelectTopNRows command from SSMS  ******/
+SELECT DISTINCT -- [SalesQuotaKey]
+      --,[EmployeeKey]
+      --,[DateKey]
+      --,[CalendarYear]
+      --,[CalendarQuarter]
+      --,[SalesAmountQuota]
+      Date
+  FROM [AdventureWorksDW2019].[dbo].[FactSalesQuota]
