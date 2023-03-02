d40f3de289089e682ed556fdf4991f0ecd8b7522
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-02 12:08:05 +0300

    distinct currncy table

---
+++ 2  SELECT DISTINCT/currrencydate.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT DISTINCT --,[AverageRate]
+     -- ,[CurrencyID]
+      CurrencyDate
+      --,[EndOfDayRate]
+      --,[CurrencyKey]
+      --,[DateKey]
+  FROM [AdventureWorksDW2019].[dbo].[NewFactCurrencyRate]
