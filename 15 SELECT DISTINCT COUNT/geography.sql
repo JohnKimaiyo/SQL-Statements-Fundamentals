062ea2f4c982c6e883a8ce2148baf70795f2d082
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-03 12:35:01 +0300

    geography table distinct count sql

---
+++ 15 SELECT DISTINCT COUNT/geography.sql
@@ -0,0 +1,13 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT COUNT (DISTINCT City) --, [GeographyKey]
+      --,[City]
+      --,[StateProvinceCode]
+      --,[StateProvinceName]
+      --,[CountryRegionCode]
+      --,[EnglishCountryRegionName]
+      --,[SpanishCountryRegionName]
+      --,[FrenchCountryRegionName]
+      --,[PostalCode]
+      --,[SalesTerritoryKey]
+      --,[IpAddressLocator]
+  FROM [AdventureWorksDW2019].[dbo].[DimGeography]
