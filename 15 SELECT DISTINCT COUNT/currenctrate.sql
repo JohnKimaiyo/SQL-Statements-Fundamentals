6e4a01e6fc828bb9e7f7e090865270d8313c1221
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-03 15:48:07 +0300

    currency rate sql table

---
+++ 15 SELECT DISTINCT COUNT/currenctrate.sql
@@ -0,0 +1,8 @@
+tTopNRows command from SSMS  ******/
+SELECT COUNT   (DISTINCT AverageRate)
+      --,[CurrencyID]
+      --,[CurrencyDate]
+      --,[EndOfDayRate]
+      --,[CurrencyKey]
+      --,[DateKey]
+  FROM [AdventureWorksDW2019].[dbo].[NewFactCurrencyRate]
