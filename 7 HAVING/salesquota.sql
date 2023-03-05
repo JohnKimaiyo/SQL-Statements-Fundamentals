ad6878051c3ec7dd8bf572fe6dc5f87b491cbdab
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-05 20:54:53 +0300

    salequota having count

---
+++ 7 HAVING/salesquota.sql
@@ -0,0 +1,6 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT COUNT  (CalendarYear) ,CalendarYear
+      
+  FROM [AdventureWorksDW2019].[dbo].[FactSalesQuota]
+  GROUP BY CalendarYear
+ HAVING COUNT  (CalendarYear) >5;
