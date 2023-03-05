141a839c201d10c716b099c785eed3b72c843bba
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-05 20:58:30 +0300

    survey response having count

---
+++ 7 HAVING/surveyresponse.sql
@@ -0,0 +1,6 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT COUNT (EnglishProductSubcategoryName) ,EnglishProductSubcategoryName
+
+  FROM [AdventureWorksDW2019].[dbo].[FactSurveyResponse]
+  GROUP BY EnglishProductSubcategoryName
+  HAVING COUNT(EnglishProductSubcategoryName)>150;
