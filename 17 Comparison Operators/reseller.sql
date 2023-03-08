68aa73db728ca26bd07e6a77c1b18e7b31b28a0b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-08 17:01:27 +0300

    reseller sql  >-

---
+++ 17 Comparison Operators/reseller.sql
@@ -0,0 +1,23 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT ResellerKey,
+      GeographyKey,
+      ResellerAlternateKey,
+      Phone,
+      BusinessType,
+      ResellerName,
+      NumberEmployees,
+      OrderFrequency,
+      OrderMonth,
+      FirstOrderYear,
+      LastOrderYear
+      ProductLine,
+      AddressLine1,
+      AddressLine2,
+      AnnualSales,
+      BankName,
+      MinPaymentType,
+      MinPaymentAmount,
+      AnnualRevenue,
+      YearOpened
+  FROM [AdventureWorksDW2019].[dbo].[DimReseller]
+  WHERE AnnualRevenue >=50000;
