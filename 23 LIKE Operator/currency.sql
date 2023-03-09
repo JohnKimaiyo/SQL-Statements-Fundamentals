4f7fcac9e8850cd684cbbb71d54b9cb5fbc6ab3b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-09 14:11:16 +0300

    currncy sql

---
+++ 23 LIKE Operator/currency.sql
@@ -0,0 +1,6 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CurrencyKey]
+      ,[CurrencyAlternateKey]
+      ,[CurrencyName]
+  FROM [AdventureWorksDW2019].[dbo].[DimCurrency]
+  WHERE CurrencyName LIKE '%Dollar';
