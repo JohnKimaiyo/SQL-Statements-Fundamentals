ca7e03c1bd1ba6813c234f0d1d00a5f0e856e237
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-10 11:20:27 +0300

    sales quota date dec order

---
+++ 23 LIKE Operator/salesquota.sql
@@ -0,0 +1,9 @@
+command from SSMS  ******/
+SELECT TOP (1000) [SalesQuotaKey]
+      ,[EmployeeKey]
+      ,[DateKey]
+      ,[CalendarYear]
+      ,[CalendarQuarter]
+      ,[SalesAmountQuota]
+      ,[Date]
+  FROM [AdventureWorksDW2019].[dbo].[FactSalesQuota]
